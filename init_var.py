bfead6b55b1aa12c373b937a6eb6e944c712a5eb
Alex Sidorov <alex.sidorof@ya.ru>
2023-04-05 16:38:31 +0300

    Обновил переменые дефолтные

--- init_var.py
+++ init_var.py
@@ -9,12 +9,14 @@ DEFAULT_PATH = os.path.join(appdirs.user_data_dir(), "propusk")
 DEFAULT_PATH_PHOTO = os.path.join(DEFAULT_PATH, "photo")
 
 no_media = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'image', 'no_media_main.jpg')
+
 os.environ.update({
     'VERSION': "ver 0.4 beta",
     'ABSOLUTE_PATH': os.path.dirname(os.path.abspath(__file__)),
     'NO_MEDIA_IMAGE': no_media,
     'INDEX_PHOTO': '0',
-    'INDEX_CAMERA': '1'
+    'INDEX_CAMERA': '1',
+    'DEFAULT_PATH': '/home/asidorov/Документы/propusk_db'
 })
 
 def load_variables():
